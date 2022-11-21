e9a47eeec3e3b20dbbf279dbcf506a97894b4b19
Fiard <dominoi@yandex.ru>
2022-11-21 18:17:28 +0300

    Some default .net initial sln/project

---
+++ PD2Status/PD2StatusWin/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace PD2StatusWin
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        private void OnXamlStartup(object sender, StartupEventArgs e)
+        {
+            _startup();
+        }
+
+        private void _startup()
+        {
+            Current.ShutdownMode = ShutdownMode.OnMainWindowClose; //re-enable normal shutdown mode
+            var mainWindow = new MainWindow();
+            Current.MainWindow = mainWindow;
+
+            mainWindow.Show();
+        }
+    }
+}
