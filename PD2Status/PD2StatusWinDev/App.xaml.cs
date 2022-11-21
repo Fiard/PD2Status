a8ecfc9d87ba841414c582164e691c32044ff2d5
Fiard <dominoi@yandex.ru>
2022-11-21 18:56:42 +0300

    default win/model dummy interactions

--- PD2Status/PD2StatusWinDev/App.xaml.cs
+++ PD2Status/PD2StatusWinDev/App.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows;
+using Nova.Core.Log;
+using Nova.Kernel.Shared.Log;
 
 namespace PD2StatusWin
 {
@@ -7,6 +12,19 @@ namespace PD2StatusWin
     /// </summary>
     public partial class App
     {
+        private static readonly LogLib _log = new LogLib(nameof(App));
+
+        public App()
+        {
+            var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var persistentDataPath = Path.Combine(appData, "Fiard", "PD2Status");
+            
+            CoreLogManager.initializeCoreLogs(null, Path.GetFullPath(Path.Combine(persistentDataPath, "Logs")));
+            _log.report(">>>>>>>> PD2 Status <<<<<<<<");
+            LogManager.replaceProcessId("pd2s");
+            LogManager.replaceThreadId(Thread.CurrentThread.ManagedThreadId, "--");
+        }
+
         private void OnXamlStartup(object sender, StartupEventArgs e)
         {
             _startup();
---
+++ PD2Status/PD2StatusWinDev/Main/PD2SModel.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace PD2StatusWin.Main;
+
+public class PD2SModel : DependencyObject
+{
+    public static PD2SModel instance { get; } = new();
+}
